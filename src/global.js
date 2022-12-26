fd0fce7bdecea4dab6a57aca467d315c6a2cd65b
devAshokC <ashokkumarchinna27@gmail.com>
2022-12-26 19:10:46 +0530

    Commited

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
 // export const API = `https://6322ddef362b0d4e7dd51013.mockapi.io/movies`;
-export const API = `http://localhost:4030/movies`;
+export const API = `https://movieapp-nx3q.onrender.com/movies`;
 // export const API = `https://b-39-wd-tamil-node-production.up.railway.app/movies`;
