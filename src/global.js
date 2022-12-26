1ac290b93708aab79885761f5f879f1db3145686
devAshokC <ashokkumarchinna27@gmail.com>
2022-12-26 19:11:43 +0530

    updated

--- src/global.js
+++ src/global.js
@@ -1,3 +1,1 @@
-// export const API = `https://6322ddef362b0d4e7dd51013.mockapi.io/movies`;
 export const API = `https://movieapp-nx3q.onrender.com/movies`;
-// export const API = `https://b-39-wd-tamil-node-production.up.railway.app/movies`;
